60b478bc56414586a4ce85c8475b83143f028329
olimpio kaio <olimpiocode@gmail.com>
2024-03-05 20:43:28 -0300

    Commit inicial, estrutura projeto

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUlCPfHnxXHC0PNSewiUtEFbeKLe76L_bIoPFzI8JQnUwOslGah-T01B9mOWOAIjiyo_k&usqp=CAU'),
+
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUlCPfHnxXHC0PNSewiUtEFbeKLe76L_bIoPFzI8JQnUwOslGah-T01B9mOWOAIjiyo_k&usqp=CAU')
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrl: './shopping-list.component.css'
+})
+export class ShoppingListComponent {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 5)
+  ];
+
+  constructor() {
+  }
+
+}
