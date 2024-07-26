4d295b196d6ef62ecbbf221beadaf5cf33709242
Olimpio Kaio <olimpiocode@gmail.com>
2024-07-26 11:49:44 -0300

    adicionando ingredientes e receitas

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -9,15 +8,7 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService],
 })
 export class RecipesComponent {
-  selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-      this.selectedRecipe = recipe;
-    });
-  }
+  constructor() { }
 
-  onRecipeDetail(recipeEl: Recipe) {
-    this.selectedRecipe = recipeEl;
-  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,14 +1,15 @@
 import { Injectable, EventEmitter, Output } from '@angular/core';
 import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class ShoppingListService {
 
-  @Output() ingredientsChanged = new EventEmitter<Ingredient[]>();
+  @Output() ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apple', 5),
-    new Ingredient('Tomatoes', 5)
+    new Ingredient('Tomatoes', 5),
   ];
 
   public getIngredients() {
@@ -17,12 +18,11 @@ export class ShoppingListService {
 
   public addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice())
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
-
 }
